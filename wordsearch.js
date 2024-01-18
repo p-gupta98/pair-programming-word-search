c8b5355002c9c1dfea1f04887fd417992aa21fee
vagrant <pihugupta0501@gmail.com>
2024-01-18 00:58:02 +0000

    added more test cases and modified function to pass tests

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const vertical = function(arr, wordToFind) {
+  let result = [];
+  for (const keyArray in arr) {
+    for (const value in arr[keyArray]) {
+      if (!result[value]) {
+        result[value] = [];
+      }
+      result[value][keyArray] = arr[keyArray][value];
     }
-}
+  }
+  
+  const verticalJoin = result.map(word => word.join(''));
+  for (let l of verticalJoin) {
+    if (l.includes(wordToFind)) return true;
+  }
+  return false;
+};
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  if (!letters || !word) {
+    return false;
+  }
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  return vertical(letters, word);
+};
+
+module.exports = wordSearch;
